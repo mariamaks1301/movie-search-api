844653451634276d0674cc1e3486562802dea99b
mariamaks1301 <mariamaks1301@gmail.com>
2023-06-02 19:06:09 +0600

    prepare to deploy

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -6,9 +6,7 @@ const Header = () => {
             <div className="nav-wrapper #616161 grey darken-2 container">
             <a href="#" className="brand-logo">MOVIES</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">JavaScript</a></li>
+                <li><a href="!#">Repo</a></li>              
             </ul>
             </div>
         </nav>
